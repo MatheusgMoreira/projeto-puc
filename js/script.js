2cc39139dfa2ce8ebbb4d98565b1fffdf271f126
Matheus Moreira <matheusmoreira1324@hotmail.com>
2022-04-18 21:12:30 -0300

    projeto-puc

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 import initAccordion from './modules/accordion.js';
 import initModal from './modules/modal.js'
+import initTimer from './modules/timer.js'
 
 initModal();
 initAccordion();
+initTimer();
