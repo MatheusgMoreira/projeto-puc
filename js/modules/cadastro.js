e5ed0d7c163dea9525492e1098a2ec124272b6fc
Matheus Moreira <matheusmoreira1324@hotmail.com>
2022-04-23 12:44:34 -0300

    projeto-puc

--- js/modules/cadastro.js
+++ js/modules/cadastro.js
@@ -11,7 +11,7 @@ class Pessoa {
   validarDados() {
     //Verifica se as informações foram preenchidas
     for (let i in this) {
-      if (this[i] == undefined || this[i] == "" || this[i] == null) {
+      if (this[i] == undefined || this[i] == '' || this[i] == null) {
         return false;
       }
     }
@@ -22,108 +22,110 @@ class Pessoa {
 class Bd {
   //Criação da id do Banco de dados no localStorage
   constructor() {
-    let id = localStorage.getItem("id");
+    let id = localStorage.getItem("id")
 
     if (id === null) {
-      localStorage.setItem("id", 0);
+      localStorage.setItem('id', 0)
     }
   }
 
   getProximoId() {
-    let proximoId = localStorage.getItem("id");
-    return parseInt(proximoId) + 1;
+    let proximoId = localStorage.getItem("id")
+    return parseInt(proximoId) + 1
   }
 
   gravar(d) {
-    let id = this.getProximoId();
+    let id = this.getProximoId()
 
-    localStorage.setItem(id, JSON.stringify(d));
+    localStorage.setItem(id, JSON.stringify(d))
 
-    localStorage.setItem("id", id);
+    localStorage.setItem('id', id)
   }
 
   recuperarTodosRegistros() {
-    //Array de pessoa
-    let pessoas = Array();
-
-    let id = localStorage.getItem("id");
+    //Array de pessoas
+    let pessoas = Array()
+    
+    let id = localStorage.getItem('id')
 
     //Recupera todas as pessoas cadastradas em LocalStorage
     for (let i = 1; i <= id; i++) {
       //Recupera a despesa
-      let pessoa = JSON.parse(localStorage.getItem(i));
+      let pessoa = JSON.parse(localStorage.getItem(i))
 
       //Verifica a possibilidade de haver índices que foram pulados/removidos
       if (pessoa === null) {
-        continue;
+        continue
       }
-      pessoa.id = i;
-      pessoas.push(pessoa);
+      pessoa.id = i
+      pessoas.push(pessoa)
+      
     }
 
-    return pessoas;
+    return pessoas
+    
   }
   
   pesquisar(pessoa) {
 
     let pessoasFiltradas = Array();
-    pessoasFiltradas = this.recuperarTodosRegistros();
+    pessoasFiltradas = this.recuperarTodosRegistros()
 
     //Filtros do Array
 
     //nome
-    if (pessoa.nome != "") {
-      pessoasFiltradas = pessoasFiltradas.filter(d => d.nome == pessoa.nome);
+    if (pessoa.nome != '') {
+      pessoasFiltradas = pessoasFiltradas.filter(d => d.nome == pessoa.nome)
     }
 
     //Mês
-    if (pessoa.cpf != "") {
-      pessoasFiltradas = pessoasFiltradas.filter(d => d.cpf == pessoa.cpf);
+    if (pessoa.cpf != '') {
+      pessoasFiltradas = pessoasFiltradas.filter(d => d.cpf == pessoa.cpf)
     }
 
     //carro
     if (pessoa.carro != '') {
       pessoasFiltradas = pessoasFiltradas.filter(
         d => d.carro == pessoa.carro
-      );
+      )
     }
 
     //email
     if (pessoa.email != '') {
       pessoasFiltradas = pessoasFiltradas.filter(
         d => d.email == pessoa.email
-      );
+      )
     }
 
     //Descrição
     if (pessoa.telefone != '') {
       pessoasFiltradas = pessoasFiltradas.filter(
         d => d.telefone == pessoa.telefone
-      );
+      )
     }
 
     //vaga
     if (pessoa.vaga != '') {
-      pessoasFiltradas = pessoasFiltradas.filter(d => d.vaga == pessoa.vaga);
+      pessoasFiltradas = pessoasFiltradas.filter(d => d.vaga == pessoa.vaga)
     }
 
-    return pessoasFiltradas;
+    return pessoasFiltradas
   }
 
   remover(id) {
-    localStorage.removeItem(id);
+    localStorage.removeItem(id)
   }
 }
 
-let bd = new Bd();
+let bd = new Bd()
 
 function cadastrarPessoa() {
-  let nome = document.getElementById('nome');
-  let cpf = document.getElementById('cpf');
-  let carro = document.getElementById('carro');
-  let email = document.getElementById('email');
-  let telefone = document.getElementById('telefone');
-  let vaga = document.getElementById('vaga');
+  let nome = document.getElementById('nome')
+  let cpf = document.getElementById('cpf')
+  let carro = document.getElementById('carro')
+  let email = document.getElementById('email')
+  let telefone = document.getElementById('telefone')
+  let vaga = document.getElementById('vaga')
 
   let pessoa = new Pessoa(
     nome.value,
@@ -132,22 +134,23 @@ function cadastrarPessoa() {
     email.value,
     telefone.value,
     vaga.value
-  );
+  )
 
   if (pessoa.validarDados()) {
     //Diálogo de sucesso e gravação
 
-    bd.gravar(pessoa);
+    bd.gravar(pessoa)
 
-    alert('Cadastro realizado com sucesso!!!');
+    alert('Cadastro realizado com sucesso!!!')
 
     //Limpa os campos ao enviar as informações
-    nome.value = '';
-    cpf.value = '';
-    carro.value = '';
-    email.value = '';
-    telefone.value = '';
-    vaga.value = '';
+    nome.value = ''
+    cpf.value = ''
+    carro.value = ''
+    email.value = ''
+    telefone.value = ''
+    vaga.value = ''
+
   } else {
     //Diálogo de erro
     alert(
@@ -157,27 +160,45 @@ function cadastrarPessoa() {
 }
 
 function carregaListaPessoas(pessoas = Array(), filtro = false) {
+
   if (pessoas.length == 0 && filtro == false) {
-    pessoas = bd.recuperarTodosRegistros();
+    pessoas = bd.recuperarTodosRegistros()
   }
 
   //Seleciona o elemento tbody da tabela
-  let listaPessoas = document.getElementById("listaPessoas");
-  listaPessoas.innerHTML = "";
+  let listaPessoas = document.getElementById("listaPessoas")
+  listaPessoas.innerHTML = ''
 
   //Percorre o array pessoa, listando cada despesa de forma dinâmica
-  pessoas.forEach((d) => {
+  pessoas.forEach(function(d) {
 
     //Cria a linha <tr>
     var linha = listaPessoas.insertRow();
 
     //Cria as colunas <td>
-    linha.insertCell(0).innerHTML = d.nome;
-    linha.insertCell(1).innerHTML = d.cpf;
-    linha.insertCell(2).innerHTML = d.carro;
-    linha.insertCell(3).innerHTML = d.email;
-    linha.insertCell(4).innerHTML = d.telefone;
-    linha.insertCell(5).innerHTML = d.vaga;
+    linha.insertCell(0).innerHTML = d.nome
+    linha.insertCell(1).innerHTML = d.cpf
+    linha.insertCell(2).innerHTML = d.carro
+    linha.insertCell(3).innerHTML = d.email
+    linha.insertCell(4).innerHTML = d.telefone
+    linha.insertCell(5).innerHTML = d.vaga
+
+    //Botão de excluír
+  let btn = document.createElement('button')
+  btn.className = 'botao-excluir'
+  btn.innerHTML = '<i></i>'
+  btn.id = `id_pessoa_${d.id}`
+  btn.onclick = function(){
+          
+    let id = this.id.replace('id_pessoa_','')
+    
+          //Remove a despesa
+    bd.remover(id)
+    window.location.reload()
+  }
+
+  linha.insertCell(6).append(btn)
+  console.log(d)
   })
 }
 
