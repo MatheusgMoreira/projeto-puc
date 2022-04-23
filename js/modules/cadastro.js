2fdce4d764baa06095305a5664591329503a6bb5
Matheus Moreira <matheusmoreira1324@hotmail.com>
2022-04-23 11:43:20 -0300

    projeto-puc

--- js/modules/cadastro.js
+++ js/modules/cadastro.js
@@ -43,87 +43,87 @@ class Bd {
   }
 
   recuperarTodosRegistros() {
+    //Array de pessoa
+    let pessoas = Array();
 
-		//Array de pessoa
-		let pessoas = Array()
+    let id = localStorage.getItem("id");
 
-		let id = localStorage.getItem('id')
+    //Recupera todas as pessoas cadastradas em LocalStorage
+    for (let i = 1; i <= id; i++) {
+      //Recupera a despesa
+      let pessoa = JSON.parse(localStorage.getItem(i));
 
-		//Recupera todas as pessoas cadastradas em LocalStorage
-		for(let i = 1; i <= id; i++) {
-
-			//Recupera a despesa
-			let pessoa = JSON.parse(localStorage.getItem(i))
-
-			//Verifica a possibilidade de haver índices que foram pulados/removidos
-			if(pessoa === null) {
-				continue
-			}
-			pessoa.id = i
-			pessoas.push(pessoa)
-		}
-
-		return pessoas
-  }
-    pesquisar(pessoa){
-
-      let pessoasFiltradas = Array()
-      pessoasFiltradas = this.recuperarTodosRegistros()
-      console.log(pessoasFiltradas);
-      console.log(pessoa)
-  
-          //Filtros do Array
-  
-      //nome
-      if(pessoa.nome != ''){
-        console.log("filtro de nome");
-        pessoasFiltradas = pessoasFiltradas.filter(d => d.nome == pessoa.nome)
+      //Verifica a possibilidade de haver índices que foram pulados/removidos
+      if (pessoa === null) {
+        continue;
       }
-        
-      //Mês
-      if(pessoa.cpf != ''){
-        console.log("filtro de cpf");
-        pessoasFiltradas = pessoasFiltradas.filter(d => d.cpf == pessoa.cpf)
-      }
-  
-      //carro
-      if(pessoa.carro != ''){
-        console.log("filtro de carro");
-        pessoasFiltradas = pessoasFiltradas.filter(d => d.carro == pessoa.carro)
-      }
-  
-      //email
-      if(pessoa.email != ''){
-        console.log("filtro de email");
-        pessoasFiltradas = pessoasFiltradas.filter(d => d.email == pessoa.email)
-      }
-  
-      //Descrição
-      if(pessoa.telefone != ''){
-        console.log("filtro de telefone");
-        pessoasFiltradas = pessoasFiltradas.filter(d => d.telefone == pessoa.telefone)
-      }
-  
-      //vaga
-      if(pessoa.vaga != ''){
-        console.log("filtro de vaga");
-        pessoasFiltradas = pessoasFiltradas.filter(d => d.vaga == pessoa.vaga)
-      }
-
-      return pessoaFiltradas
+      pessoa.id = i;
+      pessoas.push(pessoa);
     }
+
+    return pessoas;
+  }
+  
+  pesquisar(pessoa) {
+
+    let pessoasFiltradas = Array();
+    pessoasFiltradas = this.recuperarTodosRegistros();
+
+    //Filtros do Array
+
+    //nome
+    if (pessoa.nome != "") {
+      pessoasFiltradas = pessoasFiltradas.filter(d => d.nome == pessoa.nome);
+    }
+
+    //Mês
+    if (pessoa.cpf != "") {
+      pessoasFiltradas = pessoasFiltradas.filter(d => d.cpf == pessoa.cpf);
+    }
+
+    //carro
+    if (pessoa.carro != '') {
+      pessoasFiltradas = pessoasFiltradas.filter(
+        d => d.carro == pessoa.carro
+      );
+    }
+
+    //email
+    if (pessoa.email != '') {
+      pessoasFiltradas = pessoasFiltradas.filter(
+        d => d.email == pessoa.email
+      );
+    }
+
+    //Descrição
+    if (pessoa.telefone != '') {
+      pessoasFiltradas = pessoasFiltradas.filter(
+        d => d.telefone == pessoa.telefone
+      );
+    }
+
+    //vaga
+    if (pessoa.vaga != '') {
+      pessoasFiltradas = pessoasFiltradas.filter(d => d.vaga == pessoa.vaga);
+    }
+
+    return pessoasFiltradas;
   }
 
+  remover(id) {
+    localStorage.removeItem(id);
+  }
+}
 
 let bd = new Bd();
 
 function cadastrarPessoa() {
-  let nome = document.getElementById("nome");
-  let cpf = document.getElementById("cpf");
-  let carro = document.getElementById("carro");
-  let email = document.getElementById("email");
-  let telefone = document.getElementById("telefone");
-  let vaga = document.getElementById("vaga");
+  let nome = document.getElementById('nome');
+  let cpf = document.getElementById('cpf');
+  let carro = document.getElementById('carro');
+  let email = document.getElementById('email');
+  let telefone = document.getElementById('telefone');
+  let vaga = document.getElementById('vaga');
 
   let pessoa = new Pessoa(
     nome.value,
@@ -139,80 +139,60 @@ function cadastrarPessoa() {
 
     bd.gravar(pessoa);
 
-    alert('Cadastro realizado com sucesso!!!')
+    alert('Cadastro realizado com sucesso!!!');
 
     //Limpa os campos ao enviar as informações
-    nome.value = "";
-    cpf.value = "";
-    carro.value = "";
-    email.value = "";
-    telefone.value = "";
-    vaga.value = "";
+    nome.value = '';
+    cpf.value = '';
+    carro.value = '';
+    email.value = '';
+    telefone.value = '';
+    vaga.value = '';
   } else {
     //Diálogo de erro
-
-    alert('Erro ao efetuar cadastro, verifique se todas as informações foram preenchidas corretamente')
-    
+    alert(
+      'Erro ao efetuar cadastro, verifique se todas as informações foram preenchidas corretamente'
+    );
   }
 }
 
-function carregaListaPessoas(pessoa = Array(), filtro = false) {
-
-  if(pessoa.length == 0 && filtro == false){
-  pessoa = bd.recuperarTodosRegistros() 
-}
+function carregaListaPessoas(pessoas = Array(), filtro = false) {
+  if (pessoas.length == 0 && filtro == false) {
+    pessoas = bd.recuperarTodosRegistros();
+  }
 
   //Seleciona o elemento tbody da tabela
-let listapessoa = document.getElementById("listaPessoas")
-  listapessoa.innerHTML = ''
+  let listaPessoas = document.getElementById("listaPessoas");
+  listaPessoas.innerHTML = "";
 
-   //Percorre o array pessoa, listando cada despesa de forma dinâmica
-pessoa.forEach(function(d){
+  //Percorre o array pessoa, listando cada despesa de forma dinâmica
+  pessoas.forEach((d) => {
 
-  //Cria a linha <tr>
-  var linha = listaPessoa.insertRow();
+    //Cria a linha <tr>
+    var linha = listaPessoas.insertRow();
 
-  //Cria as colunas <td>
-  linha.insertCell(0).innerHTML = d.nome 
-  linha.insertCell(1).innerHTML = d.cpf
-  linha.insertCell(2).innerHTML = d.carro
-  linha.insertCell(3).innerHTML = d.email
-  linha.insertCell(4).innerHTML = d.telefone
-  linha.insertCell(5).innerHTML = d.vaga
-
-  //Botão de excluír
-  let btn = document.createElement('button')
-  btn.className = 'tabela-button'
-  btn.innerHTML = 'excluir'
-  btn.id = `id_pessoa_${d.id}`
-  btn.onclick = function(){
-          
-    let id = this.id.replace('id_pessoa_','')
-    
-          //Remove a pessoa
-    bd.remover(id)
-    window.location.reload()
-  }
-  linha.insertCell(6).append(btn)
-  console.log(d)
-})
-
+    //Cria as colunas <td>
+    linha.insertCell(0).innerHTML = d.nome;
+    linha.insertCell(1).innerHTML = d.cpf;
+    linha.insertCell(2).innerHTML = d.carro;
+    linha.insertCell(3).innerHTML = d.email;
+    linha.insertCell(4).innerHTML = d.telefone;
+    linha.insertCell(5).innerHTML = d.vaga;
+  })
 }
 
+function pesquisarPessoa() {
 
-function pesquisarPessoa(){
- 
-let nome  = document.getElementById("nome").value
-let cpf = document.getElementById("cpf").value
-let carro = document.getElementById("carro").value
-let email = document.getElementById("email").value
-let telefone = document.getElementById("telefone").value
-let vaga = document.getElementById("vaga").value
+  let nome = document.getElementById("nome").value;
+  let cpf = document.getElementById("cpf").value;
+  let carro = document.getElementById("carro").value;
+  let email = document.getElementById("email").value;
+  let telefone = document.getElementById("telefone").value;
+  let vaga = document.getElementById("vaga").value;
 
-let pessoa = new Pessoa(nome, cpf, carro, email, telefone, vaga)
+  let pessoa = new Pessoa(nome, cpf, carro, email, telefone, vaga);
 
-let pessoas = bd.pesquisar(pessoa)
- 
-this.carregaListapessoa(pessoas, true)
+  let pessoas = bd.pesquisar(pessoa);
 
+  this.carregaListaPessoas(pessoas, true);
 }
