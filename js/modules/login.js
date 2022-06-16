d617ce99d66ad564319d9eded1c9b72bcbf4b07e
Matheus Moreira <matheusmoreira1324@hotmail.com>
2022-06-16 11:06:09 -0300

    projeto-puc

--- js/modules/login.js
+++ js/modules/login.js
@@ -4,18 +4,22 @@ localStorage.setItem("login-adm", JSON.stringify(adm));
 function login() {
   let user = document.querySelector("#user").value;
   let password = document.querySelector("#password").value;
-  const conta = Array(localStorage.getItem("lista-pessoas"));
-  const loginAdm = Array(localStorage.getItem("login-adm"));
+  const conta = JSON.parse(localStorage.getItem("lista-pessoas"));
+  //const loginAdm = Array(localStorage.getItem("login-adm"));
 
-  loginAdm.forEach((usuario) => {
+  /* loginAdm.forEach((usuario) => {
+    
     if (usuario.includes(user) && usuario.includes(password)) {
       window.open("pags/adm.html");
     }
   });
+  */
 
   conta.forEach((usuario) => {
-    if (usuario.includes(user) && usuario.includes(password)) {
+    if (usuario.email == user && usuario.cpf == password) {
       window.open("pags/usuario.html");
+      return;
     }
   });
+  alert("Usuario/senha invalidos");
 }
